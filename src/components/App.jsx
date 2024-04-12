264fd0c3a7413650806765bfadda6f0b053f337f
ramocoblisan <ramonaioana67@yahoo.com>
2024-04-12 20:49:17 +0300

    App component added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import Section from "./Section/Section";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Statistics from "./Statistics/Statistics";
+import Notifications from "./Notifications/Notifications"
+
+export class App extends Component {
+  constructor (props) {
+  super(props);
+  this.state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+}
+
+handleFeedback = (evt)=> {
+  console.log('Clicked button:', evt.target);
+  console.log('Button name:', evt.target.name);
+  const {name} = evt.target; 
+  this.setState( prevState => ( { [name] : prevState[name] + 1}));
+}
+
+render() {
+  const {good, neutral, bad} = this.state
+  const total = good + neutral + bad
+  const percentage = ((good/ total) * 100).toFixed(0) ?? 0;
+  const options =['good', 'neutral', 'bad'];
+  return(
+    <>
+    <Section title="Please leave feedback">
+    <FeedbackOptions options={options} onLeaveFeedback={this.handleFeedback}/>
+      {
+        total > 0
+         ? 
+         (<Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={`${percentage}%`}/>)
+        :
+          (<Notifications message="There is no feedback"></Notifications>)
+      }
+    </Section>
+    </>
+  )
+}
+}
